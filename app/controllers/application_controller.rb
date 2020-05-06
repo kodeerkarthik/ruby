5a59dad19beee9c68443e3ff37556abc7ede7c73
kodeerkarthik <kkrishnegowdaostb1@gmail.com>
2020-05-06 13:25:03 +0530

    profile picture remove completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:avatar, :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:avatar, :email, :password,:remove_avatar)}
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :mobile, :password, :current_password, :avatar)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :mobile, :password, :current_password, :avatar,:remove_avatar)}
   end
 end
 
