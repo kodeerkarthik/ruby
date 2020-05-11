5c46bad3fedfb6fc876d11a0eb633d79a9a13e74
kodeerkarthik <kkrishnegowdaostb1@gmail.com>
2020-05-11 09:09:59 +0530

    added cc to the mail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200505070228) do
+ActiveRecord::Schema.define(version: 20200507075149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20200505070228) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "User_id"
+  end
+
+  create_table "senders", force: :cascade do |t|
+    t.string   "email"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
