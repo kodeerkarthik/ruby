ae921992eb74228e68b18517f9d8f166f456bab6
kodeerkarthik <kkrishnegowdaostb1@gmail.com>
2020-05-20 15:53:59 +0530

    conflict resolve

--- app/controllers/manage_users_controller.rb
+++ app/controllers/manage_users_controller.rb
@@ -1,5 +1,5 @@
-class ManageUserController < ApplicationController
-  load_and_authorize_resource :class => ManageUserController
+class ManageUsersController < ApplicationController
+  # load_and_authorize_resource :class => ManageUserController
 
   def index
     @users = User.all
